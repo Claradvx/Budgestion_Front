092e88792d0a24bfd15e323e472d72aeb0f60396
cdevaux <clara.devaux31@outlook.fr>
2022-03-04 09:56:34 +0100

    Ploup

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -6,7 +6,18 @@ const Home = () => {
         <>
             <h1>Bienvenue sur Budgestion</h1>
 
-            <p>L'application de gestion de budgets partagés ! </p>
+            <h3>L'application de gestion de pour vos budgets partagés ! </h3>
+
+            <img src = 'images/Budgestion_slogan_noBG.png' 
+                alt = 'Budgestion' />
+            
+
+            <p>Budgestion est l'application qu'il te faut que pour gérer les budgets entre amis en famille, pro ou encore perso.</p>
+
+            <h3>Fini les disputes entre amis</h3>
+            <p>C'est la solution pour vous libérer des conflits d'argent entre amis pendant les vacances. 
+                Gardez un oeil sur vos dépenses et vos dettes.</p>
+
         </>
     )
 }
